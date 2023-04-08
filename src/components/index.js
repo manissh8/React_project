2399ff7a72a2b4708ef25f2fb68957209690fe02
Manish <manissh1911@gmail.com>
2023-04-08 13:08:37 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./navbar";
+import Billing from "./billing";
+import CardDeal from "./cardDeal";
+import Business from "./business";
+import Clients from "./clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./footer";
+import Testimonials from "./testimonials";
+import Hero from "./hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
